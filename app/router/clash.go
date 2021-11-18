793db8921c57885083ac23082c805aa7e0436383
blackfeather527 <blackfeather527@gmail.com>
2021-11-18 20:01:12 +0800

    Update clash.go

--- app/router/clash.go
+++ app/router/clash.go
@@ -3,7 +3,7 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 
-	"github.com/whoisix/subscribe2clash/app/api"
+	"github.com/blackfeather527/subscribe2clash/app/api"
 )
 
 func clashRouter(r *gin.Engine) {
