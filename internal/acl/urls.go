12c72f57d83ca28907659f86cafc3e8d2a6311b5
blackfeather527 <blackfeather527@gmail.com>
2021-11-18 20:10:49 +0800

    Update urls.go

--- internal/acl/urls.go
+++ internal/acl/urls.go
@@ -7,7 +7,7 @@ import (
 
 	"gopkg.in/ini.v1"
 
-	"github.com/whoisix/subscribe2clash/internal/global"
+	"github.com/blackfeather527/subscribe2clash/internal/global"
 )
 
 //go:embed config/default_rules.ini
