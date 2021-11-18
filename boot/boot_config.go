d257d9e1c22a726c4d1bf1f24f500fd2f0e0e5f4
blackfeather527 <blackfeather527@gmail.com>
2021-11-18 22:39:04 +0800

    Update boot_config.go

--- boot/boot_config.go
+++ boot/boot_config.go
@@ -3,16 +3,12 @@ package boot
 import (
 	"os"
 
-	"github.com/blackfeather527/subscribe2clash/internal/acl"
 	"github.com/blackfeather527/subscribe2clash/internal/global"
 )
 
 func generateConfig() {
-	// 配置文件相关设置
-	options := Options()
 
 	if global.GenerateConfig {
-		acl.GenerateConfig(options...)
 		os.Exit(0)
 	}
 }
